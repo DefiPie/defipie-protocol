44c0da94d75691b19353de71801c649fb3cd9dba
maxxttor <maxxttor@gmail.com>
2021-11-01 16:33:07 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,31 +2,32 @@ npx saddle deploy StandardToken 220000000000000000000000000 "DeFiPIE Token" 18 "
 npx saddle deploy PErc20Delegate
 npx saddle deploy PPIEDelegate
 npx saddle deploy PEtherDelegate
-npx saddle deploy Controller
-npx saddle deploy Unitroller
-npx saddle deploy BaseInterestRateModel 20000000000000000 850000000000000000
 npx saddle deploy Registry
 npx saddle deploy RegistryProxy "$Registry" "$PErc20Delegate"
+npx saddle deploy Controller
+npx saddle deploy Unitroller "$RegistryProxy"
+npx saddle deploy BaseInterestRateModel 20000000000000000 850000000000000000
 npx saddle deploy UniswapPriceOracle
 npx saddle deploy UniswapPriceOracleProxy "$UniswapPriceOracle" "$RegistryProxy" "0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f" "0xc778417e063141139fce010982780140aa0cd5ab" "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e"
-npx saddle deploy PTokenFactory "$RegistryProxy" 1000000000000000000 "$UniswapPriceOracleProxy" "$Unitroller" "$BaseInterestRateModel" 20000000000000000 100000000000000000
-npx saddle deploy Maximillion "$PEther"
+npx saddle deploy PTokenFactory "$RegistryProxy" 1000000000000000000 "$Unitroller" "$BaseInterestRateModel" 20000000000000000 100000000000000000
 npx saddle deploy ClaimCalc "$Unitroller"
+npx saddle deploy CalcPoolPrice "$UniswapPriceOracleProxy"
+npx saddle deploy Maximillion "$PEther"
 npx saddle deploy Timelock "$admin" 172800
 npx saddle deploy Governor "$Timelock" "$RegistryProxy" "$guardian"
-npx saddle deploy CalcPoolPrice "$UniswapPriceOracleProxy"
 
 npx -n --experimental-repl-await saddle console
 
 await unitroller.methods._setPendingImplementation(controller._address).send()
 await controller.methods._become(unitroller._address).send()
 let unitroller1 = new web3.eth.Contract(controller._jsonInterface, unitroller._address)
-await unitroller1.methods._setPriceOracle(uniswapPriceOracleProxy._address).send({ from: "$from" })
-await unitroller1.methods._setFactoryContract(pTokenFactory._address).send({ from: "$from" })
 await unitroller1.methods._setLiquidationIncentive('1080000000000000000').send({ from: "$from" })
 await unitroller1.methods._setMaxAssets(20).send({ from: "$from" })
 await unitroller1.methods._setCloseFactor('500000000000000000').send({ from: "$from" })
+await unitroller1.methods._setLiquidateGuardian('0x6EBD066e2891922f3c1BfD426B2a49D5D0bc11C8').send({ from: "$from" })
+await unitroller1.methods._setPauseGuardian('0x6EBD066e2891922f3c1BfD426B2a49D5D0bc11C8').send({ from: "$from" })
 let registry1 = new web3.eth.Contract(registry._jsonInterface, registryProxy._address)
 await registry1.methods._setFactoryContract(pTokenFactory._address).send({ from: "$from" })
+await registry1.methods.setOracle(uniswapPriceOracleProxy._address).send({ from: "$from" })
 await pTokenFactory.methods.createPETH(pEtherDelegate._address).send()
 await pTokenFactory.methods.createPPIE("0xb36afc9f38d8ac6f991bb9939d3ee8d45a7a1285", pPIEDelegate._address).send()
--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -30,30 +30,56 @@ const RegistryData = require('../build/contracts/Registry.json');
 
 module.exports = async function(deployer, network, accounts) {
 
-    await deployer.deploy(Pie, accounts[0]);
-    const PieContractInstance = await Pie.deployed();
-    console.log('Pie', PieContractInstance.address);
+    // await deployer.deploy(Pie, accounts[0]);
+    // const PieContractInstance = await Pie.deployed();
+    // console.log('Pie', PieContractInstance.address);
 
+    // PErc20Delegate:
+    await deployer.deploy(PErc20Delegate);
+    const PErc20DelegateContractInstance = await PErc20Delegate.deployed();
+    console.log('PErc20Delegate', PErc20DelegateContractInstance.address);
+    // ---
+
+    // PPIEDelegate:
     await deployer.deploy(PPIEDelegate);
     const PPIEDelegateContractInstance = await PPIEDelegate.deployed();
     console.log('PPIEDelegate', PPIEDelegateContractInstance.address);
+    // ---
 
+    // PEtherDelegate:
     await deployer.deploy(PEtherDelegate);
     const PEtherDelegateContractInstance = await PEtherDelegate.deployed();
     console.log('PEtherDelegate', PEtherDelegateContractInstance.address);
+    // ---
 
+    // Registry:
+    await deployer.deploy(Registry);
+    const RegistryContractInstance = await Registry.deployed();
+    console.log('Registry', RegistryContractInstance.address);
+    // ---
+
+    // RegistryProxy:
+    await deployer.deploy(RegistryProxy,
+        RegistryContractInstance.address,
+        PErc20DelegateContractInstance.address
+    );
+    const RegistryProxyContractInstance = await RegistryProxy.deployed();
+    console.log('RegistryProxy', RegistryProxyContractInstance.address);
+    // ---
+
+    // Controller:
     await deployer.deploy(Controller);
     const ControllerContractInstance = await Controller.deployed();
     console.log('Controller', ControllerContractInstance.address);
+    // ---
 
-    await deployer.deploy(Unitroller);
+    // Unitroller:
+    await deployer.deploy(Unitroller, RegistryProxyContractInstance.address);
     const UnitrollerContractInstance = await Unitroller.deployed();
     console.log('Unitroller', UnitrollerContractInstance.address);
+    // ---
 
-    await deployer.deploy(ClaimCalc, UnitrollerContractInstance.address);
-    const ClaimCalcInstance = await ClaimCalc.deployed();
-    console.log('ClaimCalc', ClaimCalcInstance.address);
-
+    // BaseInterestRateModel:
     let baseRatePerYear = new BigNumber('20000000000000000');
     let multiplierPerYear = new BigNumber('850000000000000000');
 
@@ -63,47 +89,31 @@ module.exports = async function(deployer, network, accounts) {
     );
     const BaseInterestRateModelContractInstance = await BaseInterestRateModel.deployed();
     console.log('BaseInterestRateModel', BaseInterestRateModelContractInstance.address);
+    // ---
 
-    await deployer.deploy(Registry);
-    const RegistryContractInstance = await Registry.deployed();
-    console.log('Registry', RegistryContractInstance.address);
-
-    await deployer.deploy(PErc20Delegate);
-    const PErc20DelegateContractInstance = await PErc20Delegate.deployed();
-    console.log('PErc20Delegate', PErc20DelegateContractInstance.address);
-
-    await deployer.deploy(RegistryProxy,
-        RegistryContractInstance.address,
-        PErc20DelegateContractInstance.address
-    );
-    const RegistryProxyContractInstance = await RegistryProxy.deployed();
-    console.log('RegistryProxy', RegistryProxyContractInstance.address);
-
+    // UniswapPriceOracle:
     await deployer.deploy(UniswapPriceOracle);
     const UniswapPriceOracleInstance = await UniswapPriceOracle.deployed();
     console.log('UniswapPriceOracle', UniswapPriceOracleInstance.address);
+    // ---
 
-    let PancakeFactoryAddress = '0xbcfccbde45ce874adcb698cc183debcf17952812';
-    let WETHPancakeAddress = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c';
-    let ChainlinkAddress = '0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE';
-
-    let RegistryProxyAddress = '0xddf147af5Bb55B1bE9bf6c37BFA5FEAf08Ac6Abd';
-    let oracle = '0x826a527F5201dFd5a0Add51637faf30a82eA51AE';
+    // UniswapPriceOracleProxy:
+    let PancakeFactoryAddress = '0x2Ce81910A61a15b1360c57267319A3F474521D2a';
+    let WETHPancakeAddress = '0xae13d989dac2f0debff460ac112a837c89baa7cd';
+    let ChainlinkAddress = '0x040b8BEF50f4635E5a8e13356Ab82951e6Aa3EA8';
 
     await deployer.deploy(UniswapPriceOracleProxy,
-        oracle,
-        RegistryProxyAddress,
+        UniswapPriceOracleInstance.address,
+        RegistryProxyContractInstance.address,
         PancakeFactoryAddress,
         WETHPancakeAddress,
         ChainlinkAddress
     );
     const UniswapPriceOracleProxyContractInstance = await UniswapPriceOracleProxy.deployed();
     console.log('UniswapPriceOracleProxy', UniswapPriceOracleProxyContractInstance.address);
+    // ---
 
-    await deployer.deploy(CalcPoolPrice, UniswapPriceOracleProxyContractInstance.address);
-    const CalcPoolPriceInstance = await CalcPoolPrice.deployed();
-    console.log('CalcPoolPrice', CalcPoolPriceInstance.address);
-
+    // PTokenFactory:
     let minUniswapLiquidity= new BigNumber('1000000000000000000');
     let initialExchangeRateMantissa = new BigNumber('20000000000000000');
     let initialReserveFactorMantissa = new BigNumber('100000000000000000');
@@ -111,7 +121,6 @@ module.exports = async function(deployer, network, accounts) {
     await deployer.deploy(PTokenFactory,
         RegistryProxyContractInstance.address,
         minUniswapLiquidity.toString(),
-        UniswapPriceOracleProxyContractInstance.address,
         UnitrollerContractInstance.address,
         BaseInterestRateModelContractInstance.address,
         initialExchangeRateMantissa.toString(),
@@ -119,22 +128,36 @@ module.exports = async function(deployer, network, accounts) {
     );
     const PTokenFactoryContractInstance = await PTokenFactory.deployed();
     console.log('PTokenFactory', PTokenFactoryContractInstance.address);
+    // ---
+
+    // ClaimCalc:
+    await deployer.deploy(ClaimCalc, UnitrollerContractInstance.address);
+    const ClaimCalcInstance = await ClaimCalc.deployed();
+    console.log('ClaimCalc', ClaimCalcInstance.address);
+    // ---
+
+    // CalcPoolPrice:
+    await deployer.deploy(CalcPoolPrice, UniswapPriceOracleProxyContractInstance.address);
+    const CalcPoolPriceInstance = await CalcPoolPrice.deployed();
+    console.log('CalcPoolPrice', CalcPoolPriceInstance.address);
+    // ---
 
     await UnitrollerContractInstance._setPendingImplementation(ControllerContractInstance.address);
     await ControllerContractInstance._become(UnitrollerContractInstance.address);
 
     let unitroller1 = new web3.eth.Contract(ControllerData.abi, UnitrollerContractInstance.address);
-    await unitroller1.methods._setPriceOracle(UniswapPriceOracleProxyContractInstance.address).send({from: accounts[0]});
-    await unitroller1.methods._setFactoryContract(PTokenFactoryContractInstance.address).send({from: accounts[0]});
     await unitroller1.methods._setLiquidationIncentive('1080000000000000000').send({from: accounts[0]});
     await unitroller1.methods._setMaxAssets(20).send({from: accounts[0]});
     await unitroller1.methods._setCloseFactor('500000000000000000').send({from: accounts[0]});
+    await unitroller1.methods._setLiquidateGuardian('0x6EBD066e2891922f3c1BfD426B2a49D5D0bc11C8').send({from: accounts[0]});
+    await unitroller1.methods._setPauseGuardian('0x6EBD066e2891922f3c1BfD426B2a49D5D0bc11C8').send({from: accounts[0]});
 
     let registry1 = new web3.eth.Contract(RegistryData.abi, RegistryProxyContractInstance.address);
     await registry1.methods._setFactoryContract(PTokenFactoryContractInstance.address).send({from: accounts[0]});
+    await registry1.methods.setOracle(UniswapPriceOracleProxyContractInstance.address).send({from: accounts[0]});
 
     await PTokenFactoryContractInstance.createPETH(PEtherDelegateContractInstance.address);
-    await PTokenFactoryContractInstance.createPPIE(PieContractInstance.address, PPIEDelegateContractInstance.address);
+    //await PTokenFactoryContractInstance.createPPIE(PieContractInstance.address, PPIEDelegateContractInstance.address);
 
     // await deployer.deploy(Maximillion,
     //     PEther address
